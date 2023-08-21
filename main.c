20503883419edbd1f1302a2818e9eaf09808115d
kacken <kacke8320@protonmail.com>
2023-08-21 20:33:08 +0200

    added bubble down

--- main.c
+++ main.c
@@ -25,26 +25,48 @@ void bubbleUp(int index, int heap[]) {
 	}
 }
 
-int bubbleDown(int index, int heap[]) {
+
+//############ workt noch nicht ############//
+void bubbleDown(int index, int heap[]) {
+	while(lchild(index) < sizeof(heap) && heap[lchild(index)] > heap[index]) {
+		if(rchild(index) < sizeof(heap) && rchild(index) < lchild(index)) {
+			swap(heap, index, rchild(index));
+			index = rchild(index);
+		} else {
+			swap(heap, index, lchild(index));
+			index = lchild(index);
+		}
+	}
 }
 
-void printHeap(int heap[]) {
-	for(int i = 0; i < sizeof(heap) -1; i++) {
+void printHeap(int heap[], int size) {
+	for(int i = 0; i < size; i++) {
 		printf("%d ", heap[i]);
 	}
 	printf("\n");
 }
 
-
+/*
+ * int getHeapSize(int heap[]) {
+ * 	return (sizeof(heap) / sizeof(heap[0]));
+ * }
+ *
+ */
 
 
 void test() {
 	int heap[] = {60, 100, 70, 30, 20, 60, 500};
-	printHeap(heap);
+	printHeap(heap, 7);
 	bubbleUp(1, heap);
-	printHeap(heap);
+	printHeap(heap, 7);
 	bubbleUp(6, heap);
-	printHeap(heap);
+	printHeap(heap, 7);
+
+	int heap2[] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200};
+	printf("%ld \n", heap2[20]);
+	printHeap(heap2, 20);
+	bubbleDown(0, heap2);
+	printHeap(heap2, 20);
 }
 
 int main() {
